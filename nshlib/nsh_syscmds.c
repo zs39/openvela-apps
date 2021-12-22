d531fc98ee3b1b2a1fd6defef5d990cc745abab4
ligd <liguiding1@xiaomi.com>
2021-12-22 19:50:48 +0800

    apps: add rptun /dev/rptun/xx panic cmd
    Change-Id: I68550bdd95ddf6f5e8d89448334e2cc8bbe65de3

--- nshlib/nsh_syscmds.c
+++ nshlib/nsh_syscmds.c
@@ -28,7 +28,6 @@
 #include <sys/boardctl.h>
 #include <sys/ioctl.h>
 #include <sys/utsname.h>
-#include <dirent.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
@@ -320,18 +319,41 @@ int cmd_reboot(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
 }
 #endif
 
+#if defined(CONFIG_BOARDCTL_RESET_CAUSE) && !defined(CONFIG_NSH_DISABLE_RESET_CAUSE)
+int cmd_reset_cause(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
+{
+  int ret;
+  struct boardioc_reset_cause_s cause;
+
+  memset(&cause, 0, sizeof(cause));
+  ret = boardctl(BOARDIOC_RESET_CAUSE, &cause);
+  if (ret < 0)
+    {
+      nsh_error(vtbl, g_fmtcmdfailed, argv[0], "boardctl", NSH_ERRNO);
+      return ERROR;
+    }
+
+  nsh_output(vtbl, "cause:0x%x, flag:0x" PRIx32 "\n",
+             cause.cause, cause.flag);
+  return OK;
+}
+#endif
+
 /****************************************************************************
  * Name: cmd_rptun
  ****************************************************************************/
 
 #if defined(CONFIG_RPTUN) && !defined(CONFIG_NSH_DISABLE_RPTUN)
-static int cmd_rptun_once(FAR struct nsh_vtbl_s *vtbl,
-                          FAR const char *path, char **argv)
+int cmd_rptun(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
 {
-  struct rptun_ping_s ping;
-  unsigned long val = 0;
-  int cmd;
   int fd;
+  int cmd;
+
+  if (argc < 3)
+    {
+      nsh_output(vtbl, g_fmtargrequired, argv[0]);
+      return ERROR;
+    }
 
   if (strcmp(argv[1], "start") == 0)
     {
@@ -341,32 +363,9 @@ static int cmd_rptun_once(FAR struct nsh_vtbl_s *vtbl,
     {
       cmd = RPTUNIOC_STOP;
     }
-  else if (strcmp(argv[1], "reset") == 0)
-    {
-      val = atoi(argv[3]);
-      cmd = RPTUNIOC_RESET;
-    }
   else if (strcmp(argv[1], "panic") == 0)
     {
       cmd = RPTUNIOC_PANIC;
-    }
-  else if (strcmp(argv[1], "dump") == 0)
-    {
-      cmd = RPTUNIOC_DUMP;
-    }
-  else if (strcmp(argv[1], "ping") == 0)
-    {
-      if (argv[3] == 0 || argv[4] == 0 || argv[5] == 0)
-        {
-          return ERROR;
-        }
-
-      ping.times = atoi(argv[3]);
-      ping.len   = atoi(argv[4]);
-      ping.ack   = atoi(argv[5]);
-
-      cmd = RPTUNIOC_PING;
-      val = (unsigned long)&ping;
     }
   else
     {
@@ -374,57 +373,17 @@ static int cmd_rptun_once(FAR struct nsh_vtbl_s *vtbl,
       return ERROR;
     }
 
-  fd = open(path, 0);
+  fd = open(argv[2], 0);
   if (fd < 0)
     {
-      nsh_output(vtbl, g_fmtarginvalid, path);
+      nsh_output(vtbl, g_fmtarginvalid, argv[2]);
       return ERROR;
     }
 
-  cmd = ioctl(fd, cmd, val);
+  ioctl(fd, cmd, 0);
 
   close(fd);
-
-  return cmd;
-}
-
-static int cmd_rptun_recursive(FAR struct nsh_vtbl_s *vtbl,
-                               const char *dirpath,
-                               struct dirent *entryp, void *pvarg)
-{
-  char *path;
-  int ret = ERROR;
-
-  if (DIRENT_ISDIRECTORY(entryp->d_type))
-    {
-      return 0;
-    }
-
-  path = nsh_getdirpath(vtbl, dirpath, entryp->d_name);
-  if (path)
-    {
-      ret = cmd_rptun_once(vtbl, path, pvarg);
-      free(path);
-    }
-
-  return ret;
-}
-
-int cmd_rptun(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
-{
-  if (argc < 3)
-    {
-      nsh_output(vtbl, g_fmtargrequired, argv[0]);
-      return ERROR;
-    }
-
-  if (strcmp(argv[2], "all") == 0)
-    {
-      return nsh_foreach_direntry(vtbl, "rptun", "/dev/rptun",
-                                  cmd_rptun_recursive, argv);
-    }
-
-  return cmd_rptun_once(vtbl, argv[2], argv);
+  return 0;
 }
 #endif
 
