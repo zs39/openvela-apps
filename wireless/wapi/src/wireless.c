2baeac8d3c008d881a740498bb5b1cfa91fe69c2
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-02-05 18:16:41 +0800

    libuv: Remove unsupported case from test list
    Change-Id: I6d79c777e19377f0ecf0ea0b126ecc44e969402b

--- wireless/wapi/src/wireless.c
+++ wireless/wapi/src/wireless.c
@@ -263,7 +263,6 @@ static int wapi_event_stream_extract(FAR struct wapi_event_stream_s *stream,
   switch (iwe_stream->cmd)
     {
       case SIOCGIWESSID:
-      case SIOCGIWENCODE:
       case IWEVGENIE:
         iwe->cmd = iwe_stream->cmd;
         iwe->len = offsetof(struct iw_event, u) + sizeof(struct iw_point);
@@ -330,7 +329,6 @@ static int wapi_scan_event(FAR struct iw_event *event,
         /* Reset it. */
 
         bzero(temp, sizeof(struct wapi_scan_info_s));
-        temp->encode = 0xffff;
 
         /* Save cell identifier. */
 
@@ -438,13 +436,6 @@ static int wapi_scan_event(FAR struct iw_event *event,
               }
           }
 
-        break;
-      }
-
-    case SIOCGIWENCODE:
-      {
-        info->has_encode = 1;
-        info->encode = event->u.data.flags;
         break;
       }
     }
