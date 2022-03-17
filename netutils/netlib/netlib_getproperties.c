f1ff2d0075c160ef6af5015504f7568440f733c9
xuewenliang <xuewenliang@xiaomi.com>
2022-03-17 19:28:14 +0800

    ping command field modification
    Change-Id: I94a875578d9dba30493c9b8b1c4c94b51a14bbb5

--- netutils/netlib/netlib_getproperties.c
+++ netutils/netlib/netlib_getproperties.c
@@ -72,7 +72,7 @@ int netlib_getproperties(FAR const char *ifname,
         {
           /* Copy the network interface name */
 
-          strlcpy(req.pifr_name, ifname, IFNAMSIZ);
+          strncpy(req.pifr_name, ifname, IFNAMSIZ);
 
           /* And perform the IOCTL command */
 
