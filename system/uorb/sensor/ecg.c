0b4d56030b5e125025f5b8df68f31c6c1a34e920
yintao <yintao@xiaomi.com>
2022-12-21 13:25:45 +0800

    cmocka:Add automated testing framework cmocka in testing
    Change-Id: I8657290e77cee2e25bf18ea0d0e4c3c67b90c9b4

--- system/uorb/sensor/ecg.c
+++ system/uorb/sensor/ecg.c
@@ -35,10 +35,9 @@ static void print_sensor_ecg_message(FAR const struct orb_metadata *meta,
   FAR const struct sensor_ecg *message = buffer;
   const orb_abstime now = orb_absolute_time();
 
-  uorbinfo_raw("%s:\ttimestamp: %" PRIu64 " (%" PRIu64 " us ago) ecg: %.4f "
-               "lead: %s",
+  uorbinfo_raw("%s:\ttimestamp: %" PRIu64 " (%" PRIu64 " us ago) ecg: %.4f",
                meta->o_name, message->timestamp, now - message->timestamp,
-               message->ecg, message->lead == 0 ? "off" : "on");
+               message->ecg);
 }
 #endif
 
