edd306b61de57ff72fb4023422d4f1f393e89d4c
Huang Qi <huangqi3@xiaomi.com>
2023-05-18 16:59:55 +0800

    tools: Disable wasm build if no wasm runtime enabled
    Change-Id: I10a31edfbc65fb53c66bd22ce61015dad51c539a

--- Application.mk
+++ Application.mk
@@ -324,4 +324,8 @@ distclean:: clean
 
 WASM_BUILD ?= n
 
-include $(APPDIR)/tools/Wasm.mk
+ifeq ($(WASM_BUILD),y)
+	ifneq ($(CONFIG_INTERPRETERS_WAMR)$(CONFIG_INTERPRETERS_WASM)$(CONFIG_INTERPRETERS_TOYWASM),)
+		include $(APPDIR)/tools/Wasm.mk
+	endif
+endif
