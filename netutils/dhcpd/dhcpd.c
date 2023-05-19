e1f0152182d40ceb8b2263729508305a38e95e86
Xiang Xiao <xiaoxiang@xiaomi.com>
2023-05-20 06:39:53 +0800

    Indent the define statement by two spaces
    Change-Id: If8d705416ea931d4c872eff07b1daf0c84fe594e

--- netutils/dhcpd/dhcpd.c
+++ netutils/dhcpd/dhcpd.c
@@ -335,7 +335,7 @@ static inline void dhcpd_arpupdate(FAR uint8_t *ipaddr, FAR uint8_t *hwaddr)
  ****************************************************************************/
 
 #ifdef CONFIG_NETUTILS_DHCPD_HOST
-# define dhcpd_time() time(0)
+#  define dhcpd_time() time(0)
 #elif defined(HAVE_LEASE_TIME)
 static time_t dhcpd_time(void)
 {
@@ -350,7 +350,7 @@ static time_t dhcpd_time(void)
   return ret;
 }
 #else
-# define dhcpd_time() (0)
+#  define dhcpd_time() (0)
 #endif
 
 /****************************************************************************
@@ -371,7 +371,7 @@ static inline bool dhcpd_leaseexpired(struct lease_s *lease)
     }
 }
 #else
-# define dhcpd_leaseexpired(lease) (false)
+#  define dhcpd_leaseexpired(lease) (false)
 #endif
 
 /****************************************************************************
