1efb22b163fea01f1d074a3da465daa27a15a1f6
liaoao <liaoao@xiaomi.com>
2023-08-09 19:50:33 +0800

    fdt: use CONFIG_DTC_VERSION to identify the version of tools
    Change-Id: Ie0803bad420d76ef376911b94af27c7ad94e8a90

--- examples/qencoder/qe.h
+++ examples/qencoder/qe.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- * apps/examples/qencoder/qe.h
+ * apps/examples/examples/qe/qe.h
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
