f1b3a75f717aca670c25ba575ed01eeec4ed5f62
dongjiuzhu1 <dongjiuzhu1@xiaomi.com>
2023-11-18 21:18:30 +0800

    examples/sotest/elf: rename romfs_img to run test with CONFIG_NSH_ROMFSETC
    Change-Id: I2675bce84ae69bd64b937b38175bb8aa71c9a898

--- examples/sotest/sotest_main.c
+++ examples/sotest/sotest_main.c
@@ -85,8 +85,8 @@
  ****************************************************************************/
 
 #ifdef CONFIG_EXAMPLES_SOTEST_BUILTINFS
-extern const unsigned char romfs_img[];
-extern const unsigned int romfs_img_len;
+extern const unsigned char sotest_romfs_img[];
+extern const unsigned int sotest_romfs_img_len;
 #endif
 
 extern const struct symtab_s g_sot_exports[];
@@ -126,9 +126,9 @@ int main(int argc, FAR char *argv[])
   /* Create a ROM disk for the ROMFS filesystem */
 
   desc.minor    = CONFIG_EXAMPLES_SOTEST_DEVMINOR;     /* Minor device number of the ROM disk. */
-  desc.nsectors = NSECTORS(romfs_img_len);             /* The number of sectors in the ROM disk */
+  desc.nsectors = NSECTORS(sotest_romfs_img_len);      /* The number of sectors in the ROM disk */
   desc.sectsize = SECTORSIZE;                          /* The size of one sector in bytes */
-  desc.image    = (FAR uint8_t *)romfs_img;            /* File system image */
+  desc.image    = (FAR uint8_t *)sotest_romfs_img;     /* File system image */
 
   printf("main: Registering romdisk at /dev/ram%d\n",
          CONFIG_EXAMPLES_SOTEST_DEVMINOR);
