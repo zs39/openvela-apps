adbe6bc0da06ca71fe6f7c54404d68bbe318d716
vela-mib <vela-mib@xiaomi.com>
2024-03-27 17:35:03 +0800

    fix build warning for kernel testsuites
    Change-Id: I56462c767b094291233b6cfc743d385d7f052a0f

--- testing/ostest/ostest.h
+++ testing/ostest/ostest.h
@@ -91,6 +91,10 @@
 
 int getopt_test(void);
 
+/* libc_memmem.c ************************************************************/
+
+int memmem_test(void);
+
 /* setvbuf.c ****************************************************************/
 
 #ifndef CONFIG_STDIO_DISABLE_BUFFERING
@@ -258,7 +262,7 @@ void sched_lock_test(void);
 
 /* vfork.c ******************************************************************/
 
-#if defined(CONFIG_ARCH_HAVE_VFORK) && defined(CONFIG_SCHED_WAITPID)
+#if defined(CONFIG_ARCH_HAVE_FORK) && defined(CONFIG_SCHED_WAITPID)
 int vfork_test(void);
 #endif
 
