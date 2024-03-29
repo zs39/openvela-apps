e0671bf37f3ab16c769099f7d8cc388a50ab0c4d
wangjianyu3 <wangjianyu3@xiaomi.com>
2024-03-29 20:30:43 +0800

    tools/mksymtab.sh: Check sed expression
    Change-Id: I18513a46899ad1e42edbfa0919612b7fc5a7dcaa

---
+++ graphics/lvgl/lv_porting/gpu_manager/sw/sw_draw.h
@@ -0,0 +1,41 @@
+/**
+ * @file sw_draw.h
+ *
+ */
+
+#ifndef SW_DRAW_H
+#define SW_DRAW_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+struct _lv_gpu_ctx_t;
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+struct _lv_gpu_ctx_t * sw_draw_ctx_create(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*TEMPLATE_DRAW_H*/
