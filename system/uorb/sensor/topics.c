d7b6e2da03afb924373e4e59e524f07e9360467d
ouyangxiangzhen <ouyangxiangzhen@xiaomi.com>
2024-06-12 20:27:40 +0800

    apps/benchmarks: update CMakeLists and Makefile for taclebench.
    Change-Id: I00853ef17bbfd279f6f0379a14078b4547f67b98

--- system/uorb/sensor/topics.c
+++ system/uorb/sensor/topics.c
@@ -22,8 +22,6 @@
  * Included Files
  ****************************************************************************/
 
-#include <nuttx/config.h>
-
 #include <ctype.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -38,6 +36,7 @@
 #include <sensor/dust.h>
 #include <sensor/ecg.h>
 #include <sensor/force.h>
+#include <sensor/gas.h>
 #include <sensor/gps.h>
 #include <sensor/gyro.h>
 #include <sensor/gesture.h>
@@ -80,6 +79,7 @@ static FAR const struct orb_metadata *g_sensor_list[] =
   ORB_ID(sensor_dust),
   ORB_ID(sensor_ecg),
   ORB_ID(sensor_force),
+  ORB_ID(sensor_gas),
   ORB_ID(sensor_gps),
   ORB_ID(sensor_gps_satellite),
   ORB_ID(sensor_gyro),
--- wireless/wapi/src/util.c
+++ wireless/wapi/src/util.c
@@ -275,6 +275,12 @@ FAR const char *wapi_ioctl_command_name(int cmd)
     case SIOCSIWPTAPRIO:
       return "SIOCSIWPTAPRIO";
 
+    case SIOCSIWPMKSA:
+      return "SIOCSIWPMKSA";
+
+    case SIOCGIWPMKSA:
+      return "SIOCGIWPMKSA";
+
     default:
       snprintf(g_ioctl_command_namebuf, WAPI_IOCTL_COMMAND_NAMEBUFSIZ,
                "0x%x", cmd);
@@ -505,7 +511,7 @@ int wapi_save_config(FAR const char *ifname,
       goto errout;
     }
 
-  fd = open(confname, O_RDWR | O_CREAT | O_TRUNC);
+  fd = open(confname, O_RDWR | O_CREAT | O_TRUNC, 0644);
   if (fd < 0)
     {
       ret = -errno;
