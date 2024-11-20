apache/nuttx-apps

Revert "apps/nshlib: fix unreachable code about i++"
This reverts commit 467de8ca831f671c24148e0910e0c6217ca952ee.

nshlib/nsh_session.c
@@ -70,6 +70,7 @@ int nsh_session(FAR struct console_stdio_s *pstate,
 {
   FAR struct nsh_vtbl_s *vtbl;
   int ret = EXIT_FAILURE;
+  int i;
 
   DEBUGASSERT(pstate);
   vtbl = &pstate->cn_vtbl;
@@ -115,40 +116,45 @@ int nsh_session(FAR struct console_stdio_s *pstate,
 
   /* Process the command line option */
 
-  if (strcmp(argv[1], "-h") == 0)
+  for (i = 1; i < argc; i++)
     {
-      nsh_output(vtbl, "Usage: %s [<script-path>|-c <command>]\n",
-                 argv[0]);
-      return EXIT_SUCCESS;
-    }
-  else if (strcmp(argv[1], "-c") == 0)
-    {
-      /* Process the inline command */
+      if (strcmp(argv[i], "-h") == 0)
+        {
+          nsh_output(vtbl, "Usage: %s [<script-path>|-c <command>]\n",
+                     argv[0]);
+          return EXIT_SUCCESS;
+        }
+      else if (strcmp(argv[i], "-c") == 0)
+        {
+          /* Process the inline command */
 
-      if (argc > 2)
-        {
-          return nsh_parse(vtbl, argv[2]);
+          if (i + 1 < argc)
+            {
+              return nsh_parse(vtbl, argv[i + 1]);
+            }
+          else
+            {
+              nsh_error(vtbl, g_fmtargrequired, argv[0]);
+              return EXIT_FAILURE;
+            }
         }
-      else
+      else if (argv[i][0] != '-')
         {
-          nsh_error(vtbl, g_fmtargrequired, argv[0]);
-          return EXIT_FAILURE;
+          break;
         }
-    }
-  else if (argv[1][0] == '-')
-    {
+
       /* Unknown option */
 
       nsh_error(vtbl, g_fmtsyntax, argv[0]);
       return EXIT_FAILURE;
     }
 
-  if (argc > 1)
+  if (i < argc)
     {
 #ifndef CONFIG_NSH_DISABLESCRIPT
       /* Execute the shell script */
 
-      return nsh_script(vtbl, argv[0], argv[1]);
+      return nsh_script(vtbl, argv[0], argv[i]);
 #else
       return EXIT_FAILURE;
 #endif

zs39/openvela-apps

Change-Id: I852c2e85d4c2bda03aaa973207a52f5edbf5337a

nshlib/nsh_session.c
@@ -116,21 +116,21 @@ int nsh_session(FAR struct console_stdio_s *pstate,
 
   /* Process the command line option */
 
-  if (argc > 1)
+  for (i = 1; i < argc; i++)
     {
-      if (strcmp(argv[1], "-h") == 0)
+      if (strcmp(argv[i], "-h") == 0)
         {
           nsh_output(vtbl, "Usage: %s [<script-path>|-c <command>]\n",
                      argv[0]);
           return EXIT_SUCCESS;
         }
-      else if (strcmp(argv[1], "-c") == 0)
+      else if (strcmp(argv[i], "-c") == 0)
         {
           /* Process the inline command */
 
-          if (argc > 2)
+          if (i + 1 < argc)
             {
-              return nsh_parse(vtbl, argv[2]);
+              return nsh_parse(vtbl, argv[i + 1]);
             }
           else
             {
@@ -138,23 +138,26 @@ int nsh_session(FAR struct console_stdio_s *pstate,
               return EXIT_FAILURE;
             }
         }
-      else if (argv[1][0] == '-')
+      else if (argv[i][0] != '-')
         {
-          /* Unknown option */
-
-          nsh_error(vtbl, g_fmtsyntax, argv[0]);
-          return EXIT_FAILURE;
+          break;
         }
-      else
-        {
+
+      /* Unknown option */
+
+      nsh_error(vtbl, g_fmtsyntax, argv[0]);
+      return EXIT_FAILURE;
+    }
+
+  if (i < argc)
+    {
 #ifndef CONFIG_NSH_DISABLESCRIPT
-          /* Execute the shell script */
+      /* Execute the shell script */
 
-          return nsh_script(vtbl, argv[0], argv[1]);
+      return nsh_script(vtbl, argv[0], argv[i]);
 #else
-          return EXIT_FAILURE;
+      return EXIT_FAILURE;
 #endif
-        }
     }
 
   /* Then enter the command line parsing loop */